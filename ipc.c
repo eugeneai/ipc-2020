0ef5477742adad562ec5fbc328cf823a7d8d21e4
Evgeny Cherkashin <eugeneai@irnok.net>
2020-03-04 14:48:52 +0800

    Added generator and printer functions.

--- ipc.c
+++ ipc.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
+typedef void f_t(int i, int xi);
+
+void gen(int start, int diff, int num, f_t f) {
+  for (int i=1;i<=num;i++) {
+    f(i, start);
+    start+=diff;
+  }
+}
+
+
+void printer(int i, int xi) {
+  printf("%i-th element is %i\n", i, xi);
+}
+
 int main(int argc, char *argv[])
 {
   printf("Hello, World!\n");
+  gen(1,2,20,printer);
   return 0;
 }
